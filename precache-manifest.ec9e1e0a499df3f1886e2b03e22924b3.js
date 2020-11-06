a6e70d6046c43fc3358d69a296c1ec6964c7378d
Rahul Khatri <forever_rahul16@hotmail.com>
2020-11-06 16:19:01 +1300

    Updates

---
+++ precache-manifest.ec9e1e0a499df3f1886e2b03e22924b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d7d38d90ef06dca9c1cdac78c42cf6",
+    "url": "/React-App-State/index.html"
+  },
+  {
+    "revision": "6d71712544f1beb62562",
+    "url": "/React-App-State/static/css/main.b0e96261.chunk.css"
+  },
+  {
+    "revision": "273a9155d852d5dd869e",
+    "url": "/React-App-State/static/js/2.ff8707e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-State/static/js/2.ff8707e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d71712544f1beb62562",
+    "url": "/React-App-State/static/js/main.ec031554.chunk.js"
+  },
+  {
+    "revision": "33a3dc53f52fe5712b87",
+    "url": "/React-App-State/static/js/runtime-main.719e7e5e.js"
+  }
+]);
